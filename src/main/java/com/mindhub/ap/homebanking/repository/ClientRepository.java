80cb934829d84c8a46e227f12b66a40405e595d0
fercuche <fer2.maciel@gmail.com>
2023-08-04 15:15:09 -0300

    task 1

--- src/main/java/com/mindhub/ap/homebanking/repository/ClientRepository.java
+++ src/main/java/com/mindhub/ap/homebanking/repository/ClientRepository.java
@@ -4,11 +4,8 @@ import com.mindhub.ap.homebanking.models.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import java.util.Optional;
 
 @RepositoryRestResource
 public interface ClientRepository extends JpaRepository<Client, Long> {
-
-    Optional<Client> findById(Long id);
 
 }
