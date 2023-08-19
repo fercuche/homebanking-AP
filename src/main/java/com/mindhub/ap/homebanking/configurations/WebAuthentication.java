5a5d9bc88f582c3af70bdabbd52982962c52458e
fercuche <fer2.maciel@gmail.com>
2023-08-19 17:33:00 -0300

    Created WebAuthentication class in configurations package

---
+++ src/main/java/com/mindhub/ap/homebanking/configurations/WebAuthentication.java
@@ -0,0 +1,38 @@
+package com.mindhub.ap.homebanking.configurations;
+
+import com.mindhub.ap.homebanking.models.Client;
+import com.mindhub.ap.homebanking.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebAuthentication extends GlobalAuthenticationConfigurerAdapter {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Override
+    public void init(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(inputEmail -> {
+            Client client = clientRepository.findByEmail(inputEmail);
+            if (client != null){
+                return new User(client.getEmail(), client.getPassword(), AuthorityUtils.createAuthorityList("CLIENT"));
+            } else {
+                throw new UsernameNotFoundException("Unknow user:" + inputEmail);
+            }
+        });
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
