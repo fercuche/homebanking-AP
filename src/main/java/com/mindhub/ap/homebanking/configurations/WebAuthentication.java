a7d554c0bf7a6de6f4d0ecdc6a5c12348baeef5f
fercuche <fer2.maciel@gmail.com>
2023-08-29 20:04:32 -0300

    Applied retro comment of task6

--- src/main/java/com/mindhub/ap/homebanking/configurations/WebAuthentication.java
+++ src/main/java/com/mindhub/ap/homebanking/configurations/WebAuthentication.java
@@ -24,7 +24,7 @@ public class WebAuthentication extends GlobalAuthenticationConfigurerAdapter {
         auth.userDetailsService(inputEmail -> {
             Client client = clientRepository.findByEmail(inputEmail);
             if (client != null){
-                if (client.getEmail() == "admin@admin.com"){
+                if ("admin@admin.com".equals(client.getEmail())){
                     return new User(client.getEmail(), client.getPassword(), AuthorityUtils.createAuthorityList("ADMIN", "CLIENT"));
                 }else
                     return new User(client.getEmail(), client.getPassword(), AuthorityUtils.createAuthorityList("CLIENT"));
