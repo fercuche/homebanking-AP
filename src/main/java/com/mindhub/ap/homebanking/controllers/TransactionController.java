4e218b665ecaa36891ef2b5e1ec50f9572422ca0
fercuche <fer2.maciel@gmail.com>
2023-09-08 17:00:56 -0300

    Applied corrections in task8

--- src/main/java/com/mindhub/ap/homebanking/controllers/TransactionController.java
+++ src/main/java/com/mindhub/ap/homebanking/controllers/TransactionController.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,20 +36,23 @@ public class TransactionController {
     @PostMapping("/transactions")
     public ResponseEntity<Object> makeTransaction(@RequestParam Double amount,@RequestParam String description,
                                                   @RequestParam String toAccountNumber,
-                                                  @RequestParam String fromAccountNumber){
+                                                  @RequestParam String fromAccountNumber, Authentication authentication){
         String missingParam = null;
         if (amount == null) {
-            missingParam = "Amount";
-        } else if (description.isEmpty()) {
-            missingParam = "Description";
-        } else if (toAccountNumber.isEmpty()) {
-            missingParam = "Destination Account Number";
-        } else if (fromAccountNumber.isEmpty()) {
-            missingParam = "Source Account Number";
+            missingParam = "Amount is required";
+        } else if (amount <= 0){
+            missingParam = "Amount must be higher than zero";
+        } else if (description.isBlank()) {
+            missingParam = "Description is required";
+        } else if (toAccountNumber.isBlank()) {
+            missingParam = "Destination Account Number is required";
+        } else if (fromAccountNumber.isBlank()) {
+            missingParam = "Source Account Number is required";
         }
         if (missingParam != null) {
-            return new ResponseEntity<>(missingParam + " is required", HttpStatus.FORBIDDEN);
+            return new ResponseEntity<>(missingParam, HttpStatus.FORBIDDEN);
         }
+
         if (fromAccountNumber.equals(toAccountNumber)){
             return new ResponseEntity<>("Source account can't be the same as Destination account", HttpStatus.FORBIDDEN);
         }
@@ -58,18 +60,17 @@ public class TransactionController {
             return new ResponseEntity<>("Source account doesn't exists", HttpStatus.FORBIDDEN);
         }
         if (!accountRepository.existsByNumber(toAccountNumber)){
-            return new ResponseEntity<>("Destiny account doesn't exists", HttpStatus.FORBIDDEN);
+            return new ResponseEntity<>("Destination account doesn't exists", HttpStatus.FORBIDDEN);
         }
         Account sourceAccount = accountRepository.findByNumber(fromAccountNumber);
         Account destinationAccount = accountRepository.findByNumber(toAccountNumber);
-        if (sourceAccount.getBalance() <= amount){
+        if (sourceAccount.getBalance() < amount){
             return new ResponseEntity<>("The amount of the balance account isn't sufficient", HttpStatus.FORBIDDEN);
         }
 
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Client currentClient = clientRepository.findByEmail(authentication.getName());
 
-        if (!sourceAccount.getClient().equals(currentClient)){
+        if (!currentClient.getAccounts().contains(sourceAccount)){
             return new ResponseEntity<>("Source account doesn't belong to authenticated client", HttpStatus.FORBIDDEN);
         }
 
